82da4c8a920c82731669c9d436f666ce4cb82676
Tam Le Minh <minhtambk92@gmail.com>
2017-10-30 16:36:11 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+       <v-app id="app" dark standalone>
+        <v-toolbar fixed dark>
+            <v-toolbar-title class="orange--text">The Feeling</v-toolbar-title>
+        </v-toolbar>
+        <main>
+            <v-container fluid>
+               <router-view></router-view>
+            </v-container>
+        </main>
+         <v-footer class="pa-5">
+      <span class="orange--text">© {{ new Date().getFullYear() }} Minh Tam</span>
+    </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const Face = () => import('@/components/Face.vue')
+
+Vue.use(Router)
+
+export default new Router({
+   mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Face',
+      component: Face
+    }
+  ]
+})
